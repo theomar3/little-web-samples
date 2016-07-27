63630f4e24e803cfbe0f7caabe1a4e0e6a788da0
Omar Ahmad <theomar3@gmail.com>
2016-07-27 16:17:17 -0500

    gmail toggle

---
+++ day-13-gmail-menu-toggle/gmail.js
@@ -0,0 +1,74 @@
+var inbox = document.querySelector('#inbox');
+var starred = document.querySelector('#starred');
+var important = document.querySelector('#important');
+var sentMail = document.querySelector('#sent-mail');
+var draft = document.querySelector('#draft');
+var cirlces = document.querySelector('#circles');
+
+
+function redInbox() {
+  inbox.classList.toggle('red');
+  starred.classList.remove('red');
+  important.classList.remove('red');
+  sentMail.classList.remove('red');
+  draft.classList.remove('red');
+  circles.classList.remove('red');
+
+};
+
+function redStarred() {
+  inbox.classList.remove('red');
+  starred.classList.toggle('red');
+  important.classList.remove('red');
+  sentMail.classList.remove('red');
+  draft.classList.remove('red');
+  circles.classList.remove('red');
+
+};
+
+function redImportant() {
+  inbox.classList.remove('red');
+  starred.classList.remove('red');
+  important.classList.toggle('red');
+  sentMail.classList.remove('red');
+  draft.classList.remove('red');
+  circles.classList.remove('red');
+
+};
+
+function redSentMail() {
+  inbox.classList.remove('red');
+  starred.classList.remove('red');
+  important.classList.remove('red');
+  sentMail.classList.toggle('red');
+  draft.classList.remove('red');
+  circles.classList.remove('red');
+
+};
+
+function redDraft() {
+  inbox.classList.remove('red');
+  starred.classList.remove('red');
+  important.classList.remove('red');
+  sentMail.classList.remove('red');
+  draft.classList.toggle('red');
+  circles.classList.remove('red');
+
+};
+
+function redCircles() {
+  inbox.classList.remove('red');
+  starred.classList.remove('red');
+  important.classList.remove('red');
+  sentMail.classList.remove('red');
+  draft.classList.remove('red');
+  circles.classList.toggle('red');
+
+};
+
+inbox.addEventListener('click', redInbox);
+starred.addEventListener('click', redStarred);
+important.addEventListener('click', redImportant);
+sentMail.addEventListener('click', redSentMail);
+draft.addEventListener('click', redDraft);
+circles.addEventListener('click', redCircles);
