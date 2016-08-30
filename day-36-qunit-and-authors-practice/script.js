004c0c51d3257bfcf741ef0834c217a9477d095f
Omar Ahmad <theomar3@gmail.com>
2016-08-30 10:26:33 -0500

    Finished

---
+++ day-36-qunit-and-authors-practice/script.js
@@ -0,0 +1,151 @@
+// 'use strict';
+
+'use strict';
+if (this.QUnitPractice === undefined) this.QUnitPractice = {};
+
+(function(context) {
+
+
+  var authors = [
+    { name: 'Andrew Murray', language: 'English', publishedWorks: 240 },
+    { name: 'Muhammad Muhiyyudin Ibn ul Arabi', language: 'Arabic', publishedWorks: 240 },
+    { name: 'Jallaludin Suyuti', language: 'Arabic', publishedWorks: 700 },
+    { name: 'Abu Hamid Al Ghazali', language: 'Arabic', publishedWorks: 200 },
+    { name: 'Isaac Asimov', language: 'English', publishedWorks: 506 },
+    { name: 'Jacob M. Appel', language: 'English', publishedWorks: 200 },
+    { name: 'Meish Goldish', language: 'English', publishedWorks: 300 },
+    { name: 'Kyokutei Bakin', language: 'Japanese', publishedWorks: 470 },
+    { name: 'Ursula Bloom', language: 'English', publishedWorks: 500 },
+    { name: 'Enid Blyton', language: 'English', publishedWorks: 600 },
+    { name: 'Barbara Cartland', language: 'English', publishedWorks: 722 },
+    { name: 'Darya Dontsova', language: 'Russian', publishedWorks: 140 },
+    { name: 'Alexandre Dumas', language: 'French', publishedWorks: 277 },
+    { name: 'Jacob Neusner', language: 'English', publishedWorks: 950 },
+    { name: 'Georges Simenon', language: 'French', publishedWorks: 500 }
+  ];
+
+  function arabicAuthorsCount() {
+    var filteredAuthors = [];
+
+    for (var author of authors) {
+      if (author.language === 'Arabic') {
+        filteredAuthors.push(author);
+      }
+    }
+
+    return filteredAuthors.length;
+  }
+
+  function englishAuthorsCount() {
+    var filteredAuthors = [];
+
+    for(var author of authors) {
+      if(author.language === "English") {
+        filteredAuthors.push(author);
+      }
+    }
+    return filteredAuthors.length;
+
+  }
+
+  function russianAuthorsCount() {
+    var filteredAuthors = [];
+
+    for(var author of authors) {
+      if(author.language === "Russian") {
+        filteredAuthors.push(author);
+      }
+    }
+    return filteredAuthors.length;
+  }
+
+  function over500() {
+    var filteredAuthors = [];
+
+    for(var author of authors) {
+      if(author.publishedWorks > 500) {
+        filteredAuthors.push(author);
+      }
+    }
+    return filteredAuthors.length;
+  }
+
+  function mostWorks() {
+    var authorName;
+    var workCount = 0;
+
+    for (var author of authors) {
+      if(author.publishedWorks > workCount) {
+        workCount = author.publishedWorks;
+        authorName = author.name;
+      }
+    }
+    return authorName;
+  }
+
+  function letterA() {
+    var aNames = [];
+    for(var author of authors) {
+      if(author.name.indexOf('A') === 0)
+      aNames.push(author.name);
+    }
+    return aNames.length;
+  }
+
+  function letterK() {
+    var kNames = [];
+    for(var author of authors) {
+      if(author.name.indexOf('K') === 0)
+      kNames.push(author.name);
+    }
+    return kNames.length;
+  }
+
+  function averagePublished() {
+    var total = 0;
+    for(var author of authors) {
+      var booksPublished = author.publishedWorks;
+      var totalBooks = total += booksPublished;
+    }
+    var average = Math.floor(totalBooks / booksPublished);
+    return average;
+  }
+
+  function averageEnglishBooksPublished() {
+    var total = 0;
+    for(var author of authors) {
+      if(author.language === "English") {
+        var booksPublished = author.publishedWorks;
+        var totalBooks = total += booksPublished;
+      }
+    }
+    var average = Math.floor(totalBooks / booksPublished);
+    return average;
+  }
+
+  function averageJapaneseBooksPublished() {
+    var total = 0;
+    for(var author of authors) {
+      if(author.language === "Japanese") {
+        var booksPublished = author.publishedWorks;
+        var totalBooks = total += booksPublished;
+      }
+    }
+    var average = Math.floor(totalBooks / booksPublished);
+    return average;
+  }
+
+
+
+  context.arabicAuthorsCount = arabicAuthorsCount;
+  context.englishAuthorsCount = englishAuthorsCount;
+  context.russianAuthorsCount = russianAuthorsCount;
+  context.over500 = over500;
+  context.mostWorks = mostWorks;
+  context.letterA = letterA;
+  context.letterK = letterK;
+  context.averagePublished = averagePublished;
+  context.averageEnglishBooksPublished = averageEnglishBooksPublished;
+  context.averageJapaneseBooksPublished = averageJapaneseBooksPublished;
+
+})(window.QUnitPractice);
---
+++ day-36-qunit-and-authors-practice/tests.js
@@ -0,0 +1,50 @@
+
+QUnit.test( 'count of authors who wrote in Arabic', function( assert ) {
+  var filtered = QUnitPractice.arabicAuthorsCount();
+  assert.deepEqual(filtered, 3, 'Should have returned three results.' );
+});
+
+QUnit.test( 'Count the number of English writers', function( assert ) {
+  var filtered = QUnitPractice.englishAuthorsCount();
+  assert.deepEqual(filtered, 8, 'Should have returned eight results.' );
+});
+
+QUnit.test( 'Count the number of Russian writers', function( assert ) {
+  var filtered = QUnitPractice.russianAuthorsCount();
+  assert.deepEqual(filtered, 1, 'Should have returned one results.' );
+});
+
+QUnit.test( 'Count the number of authors who wrote over 500 books', function( assert ) {
+  var filtered = QUnitPractice.over500();
+  assert.deepEqual(filtered, 5, 'Should have returned five results.' );
+});
+
+QUnit.test( 'The author with the most published works', function( assert ) {
+  var filtered = QUnitPractice.mostWorks();
+  assert.deepEqual(filtered, 'Jacob Neusner', 'Should have returned Jacob Neusner.' );
+});
+
+QUnit.test( 'Count of the authors whose name begins with the letter A', function( assert ) {
+  var filtered = QUnitPractice.letterA();
+  assert.deepEqual(filtered, 3, "Not the right number" );
+});
+
+QUnit.test( ' Count of the authors whose name begins with the letter K', function( assert ) {
+  var filtered = QUnitPractice.letterK();
+  assert.deepEqual(filtered, 1, "Not the right number" );
+});
+
+QUnit.test( 'Compute the average number of published works', function( assert ) {
+  var filtered = QUnitPractice.averagePublished();
+  assert.deepEqual(filtered, 13, "Not the right number" );
+});
+
+QUnit.test( 'Compute the average number of published works by English authors', function( assert ) {
+  var filtered = QUnitPractice.averageEnglishBooksPublished();
+  assert.deepEqual(filtered, 4, "Not the right number" );
+});
+
+QUnit.test( 'Compute the average number of published works by Japanese authors', function( assert ) {
+  var filtered = QUnitPractice.averageJapaneseBooksPublished();
+  assert.deepEqual(filtered, 1, "Not the right number" );
+});
