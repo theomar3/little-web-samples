6ea50e5354f1a979fcf71bcdf972d89781fe11ac
Omar Ahmad <theomar3@gmail.com>
2016-09-09 09:13:21 -0500

    final update

--- day-34-qunit-and-object-practice/practice.js
+++ day-34-qunit-and-object-practice/practice.js
@@ -7,9 +7,8 @@ function Car(make, model) {
   this.model = model;
   this.passengers = [];
   passengerArray = this.passengers;
-
-
 }
+
 Car.prototype.honk = function() {
   return 'beep beep';
 }
@@ -18,40 +17,35 @@ Car.prototype.addPassenger = function(passenger) {
   passengerArray.push(passenger);
 }
 
-
 Car.prototype.description = function() {
   return "This is a " + this.make + " " + this.model;
 }
 
 Car.prototype.passengerList = function() {
   if(passengerArray.length === 0) {
-    return "no passengers";
+    return 'no passengers';
   }
-  else if (passengerArray.length === 1) {
+  else if(passengerArray.length === 1) {
     return passengerArray;
   }
   else {
-    return passengerArray.slice(0, passengerArray.length-1).join(", ") + ", and " +passengerArray[passengerArray.length-1];
+    return passengerArray.split(0,passengerArray.length-1).join(', ') + ', and' + passengerArray.length[passengerArray.length-1];
   }
 }
 
 function ChevyCar() {
+
 }
 
 ChevyCar.prototype = new Car();
 
-ChevyCar.prototype.breakDown = function() {
+
+ChevyCar.prototype.breakDown() {
+
 }
+
+
 
 ChevyCar.prototype.honk = function() {
-  return "boop boop";
+  return 'boop boop';
 }
-
-
-var car = new Car("Toyota", "Tacoma");
-
-var car = new Car("Toyota", "Camry");
-
-car.addPassenger("Bob");
-car.addPassenger("Sue");
-car.addPassenger("George");
